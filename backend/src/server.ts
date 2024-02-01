b884703f7d10d80562ac77ccbd68f4be9654ef20
Uros <Uros.Zivadinovic7@gmail.com>
2024-02-01 13:11:40 +0100

    First Commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,7 @@
-import 'dotenv/config';
+import 'colors';
 import env from './util/validateEnv';
 import mongoose from 'mongoose';
-import express from 'express';
-import 'colors';
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello Inn Explorer');
-});
+import app from './app';
 
 const port = env.PORT;
 
